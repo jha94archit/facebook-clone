d7382c0cc7c7eda30736aaae30d25a8e52c02c9a
aj068102 <aj068102@cerner.net>
2021-06-15 19:44:46 +0530

    Center header icons and styles added

--- components/Header.js
+++ components/Header.js
@@ -13,7 +13,7 @@ import {
 
 function Header() {
     return (
-        <div>
+        <div className='sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md'>
             {/* Header Left */}
             <div className="flex items-center">
                 <Image src="https://links.papareact.com/5me"
@@ -24,14 +24,18 @@ function Header() {
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
                     <SearchIcon className="h-6 text-gray-600" />
                     <input className="flex ml-2 items-center bg-transparent
-                    outline-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
+                    outline-none placeholder-gray-500 flex-shrink" type="text" placeholder="Search Facebook" />
                 </div>
             </div>
 
             {/* Header Center */}
             <div className="flex justify-center flex-grow">
-                <div>
+                <div className="flex space-x-6 md:space-x-2">
                     <HeaderIcon Icon={HomeIcon} />
+                    <HeaderIcon Icon={FlagIcon} />
+                    <HeaderIcon Icon={PlayIcon} />
+                    <HeaderIcon Icon={ShoppingCartIcon} />
+                    <HeaderIcon Icon={UserGroupIcon} />
                 </div>
             </div>
             {/* Header Right */}
--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,7 +1,10 @@
-function HeaderIcon() {
+function HeaderIcon({ Icon }) {
     return (
-        <div>
-            HeaderIcon
+        <div className="flex items-center cursor-pointer md:px-10 sm:h-14
+        md:hover:bg-gray-100 rounded-xl
+        active:border-b-2 active:border-blue-500 group">
+            <Icon className="h-5 text-center sm:h-7 mx-auto
+             group-hover:text-blue-500"/>        
         </div>
     )
 }
